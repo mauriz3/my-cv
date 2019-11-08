d086acf4afe49b252341e52b3655f6d0d0cfd81f
Maurizio Rendon <maurizio.rendon@system73.com>
2019-11-08 01:14:42 +0000

    lint fix

--- pages/projects.vue
+++ pages/projects.vue
@@ -2,24 +2,30 @@
   <v-container grid-list-xl>
     <v-layout align-center justify-space-around row fill-height wrap>
       <v-flex xs12>
-        <h1 class="text-xs-center primary--text">My Projects</h1>
+        <h1 class="text-xs-center primary--text">
+          My Projects
+        </h1>
       </v-flex>
-      <v-flex xs11 md5 v-for="(item, i) in items" :key="i">
+      <v-flex v-for="(item, i) in items" :key="i" xs11 md5>
         <v-card width="100%" :elevation="7">
           <v-card-title class="primary title">
-            <v-spacer></v-spacer>
+            <v-spacer />
             {{ item.title }}
-            <v-spacer></v-spacer>
+            <v-spacer />
           </v-card-title>
-          <v-card-media :src="item.img.src" :alt="item.img.atl" :aspect-ratio="16 / 10"></v-card-media>
-          <v-card-text class="text-xs-center">{{ item.desc }}</v-card-text>
+          <v-card-media :src="item.img.src" :alt="item.img.atl" :aspect-ratio="16 / 10" />
+          <v-card-text class="text-xs-center">
+            {{ item.desc }}
+          </v-card-text>
           <v-card-actions>
-            <v-spacer></v-spacer>
+            <v-spacer />
             <v-btn class="mb-3" color="primary" :href="item.href" target="_blank">
               Visit site
-              <v-icon right dark>image_search</v-icon>
+              <v-icon right dark>
+                image_search
+              </v-icon>
             </v-btn>
-            <v-spacer></v-spacer>
+            <v-spacer />
           </v-card-actions>
         </v-card>
       </v-flex>
